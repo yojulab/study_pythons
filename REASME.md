c161d1505c3b587e2414d7ab92dc665a0cbe40bb
Sanghun Oh <otter.oh@gmail.com>
2023-12-04 17:42:39 +0900

    first commit

---
+++ REASME.md
@@ -0,0 +1,7 @@
+### 코딩 테스트
+
+|구분|소스|문제설명|출처|
+|--|--|--|--|
+|출력|[py](./docs/codingtests/2557.py)|Hello World!를 출력|[백준 2557](https://www.acmicpc.net/problem/2557)
+|
+
---
+++ docs/welcomes.py
@@ -0,0 +1,23 @@
+## 변수 선언 후 정의 시 고려점 (넣는 값이 문자 or 숫자)
+# 문자 출력
+print("Hellow, World! Sanghun Oh.") # 상수
+
+helloworld = "Hellow, World! Sanghun Oh."   # 문자형 변수
+print(helloworld)
+
+
+
+# 숫자 합산 출력
+numbers = 3 + 5 # 숫자형 변수
+print(numbers)
+
+# 데이터 타입 : 문자형 or 숫자형 등 변수에 대한 정의
+
+
+# 가정문
+print(numbers)
+
+if 5 > 2: # 묶음 기호인 :과 tab은 하나에 쌍
+    pass
+    print("Five is greater than two!")
+print("end")
