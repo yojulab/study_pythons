66753336dfa19268de5c22fa5a5a6ba264100431
Sanghun Oh <otter.oh@gmail.com>
2023-12-05 15:51:24 +0900

    add somethings

--- docs/data_types_booleans.py
+++ docs/data_types_booleans.py
@@ -18,3 +18,33 @@ str_name != "yojulab"
 # True
 str_name != "yoju lab"
 # False
+
+# 숫자에 대한 판단 ( 변수 + 부등호 + 기준값)
+# 컴퓨터 입장 
+# True = Yes = 1, False = No = 0
+5 == 4
+# False
+num_first = 5
+num_first == 4
+# False
+num_first != 4
+# True
+num_first > 4   # 초과
+# True
+num_first >= 4  # 이상
+# True
+
+# 점수에 따른 표시
+# 90점 이상 : A, 80점 초과 : B, 나머지 : F
+my_score = 90
+my_score >= 90
+# True
+my_score = 89
+my_score >= 90
+# False
+my_score > 80
+# True
+my_score = 80
+my_score > 80
+# False
+pass
--- docs/if_elses.py
+++ docs/if_elses.py
@@ -19,5 +19,31 @@ if str_name == "yoju lab" :
 if str_name != "yoju lab" :
     pass
     print("name is not {}.".format(str_name))
-    
+
+# if - else
+# num_first >= 4  -> True : 큼, False : 작음
+num_first = 5
+# if num_first >= 4 : # 무조건 둘 중 하나 표현
+if num_first >= 6 : # 무조건 둘 중 하나 표현
+    pass    # condition이 True
+    print("{}는 4보다 크다.".format(num_first))
+else :
+    pass    # condition이 False
+    print("{}는 4보다 작다.".format(num_first))
+
+# if - elif - else
+# 점수에 따른 표시
+# 90점 이상 : A, 80점 초과 : B, 나머지 : F
+# my_score = 90
+my_score = 79
+
+if my_score >= 90 : # 90점 이상 : A
+    pass
+    print("{}은 90점 이상으로 A 학점.".format(my_score))
+elif my_score > 80 :    # 80점 초과 : B
+    pass
+    print("{}은 80점 초과므로 B 학점".format(my_score))
+else :      # 나머지 : F
+    pass    
+    print("{}은 80점 이하이므로 F 학점".format(my_score))
 print("End Program!")
