b96a68fc2e9cfb7f7395609551b75490e15dd2da
Sanghun Oh <otter.oh@gmail.com>
2023-12-05 17:34:11 +0900

    add somethings

---
+++ docs/loops_whiles.py
@@ -0,0 +1,24 @@
+# condition이 True 동안 동작
+# while True :
+#     pass
+
+num_virtual = 0
+# while 정지 1 case with break문 사용
+while True :
+    pass
+    num_virtual = num_virtual + 1
+    print("{} = num_virtual + 1".format(num_virtual))
+    if num_virtual >= 5:
+        pass
+        break
+    pass
+
+# while 정지 2 case without break문 사용
+num_virtual = 0
+while num_virtual < 5 :
+    pass
+    num_virtual = num_virtual + 1
+    print("{} = num_virtual + 1".format(num_virtual))
+    pass
+
+print("End Program!")
