8a3828e5707c305d10134e0d9ddc1d79c4b61eaf
Sanghun Oh <otter.oh@gmail.com>
2023-12-05 15:00:47 +0900

    add somethings

---
+++ docs/data_types_booleans.py
@@ -0,0 +1,20 @@
+bool_flag = True    # yes 와 동일
+pass
+# 갈림길에서 '왼쪽으로 갈래?'
+# 대답 : 아니!(오른쪽을 의미)
+bool_flag = False
+type(bool_flag)
+# <class 'bool'>
+
+# 문자 판단
+# 같음에 대한 기호 '=='
+str_name = "yoju lab"   
+str_name == "yoju lab"
+# True
+str_name == "yojulab"
+# False
+## 같지 않음 기호 '!='
+str_name != "yojulab"
+# True
+str_name != "yoju lab"
+# False
---
+++ docs/if_elses.py
@@ -0,0 +1,23 @@
+# 기본 if - elif - else 구문
+if True :
+    pass
+elif True :
+    pass
+else :
+    pass
+
+# 문자 비교
+str_name = "yoju lab"
+
+# 질문 형식(condition) = 변수 + 부등호 + 기준값
+# 문자에 대한 긍정으로 질문
+if str_name == "yoju lab" :
+    pass
+    print("name is {}.".format(str_name))
+
+# 문자에 대한 부정으로 질문
+if str_name != "yoju lab" :
+    pass
+    print("name is not {}.".format(str_name))
+    
+print("End Program!")
