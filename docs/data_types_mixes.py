96d72820ef43331ea467a835a51dff4cc345923c
Sanghun Oh <otter.oh@gmail.com>
2023-12-14 17:15:57 +0900

    modify somethings

--- docs/data_types_mixes.py
+++ docs/data_types_mixes.py
@@ -44,3 +44,17 @@ dict_carinfo_k5 = {
     }
 list_carinfor.append(dict_carinfo_k5)
 pass
+
+# list_carinfor안에 있는 index 0번에 있는 "model": "Mustang" 접근 방법
+dict_carinfor_index_first = list_carinfor[0]
+dict_carinfor_index_first['model']
+pass
+list_carinfor[0]['model']
+pass
+
+# for로 각 dict 정보 출력
+for dict_carinfor in list_carinfor:
+    brand = dict_carinfor['brand']
+    model = dict_carinfor['model']
+    print('브렌드 : {}, 모델 : {}'.format(brand, model))
+    pass
---
+++ docs/functions_importsfunctions.py
@@ -0,0 +1,6 @@
+import functions_parameters
+
+# import functions in another file
+num_result = functions_parameters.add(4, 5)
+print("result : {0}".format(num_result))
+pass
